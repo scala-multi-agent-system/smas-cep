de295b8cb3a1e5310b1208a08f516e4f445959be
Benjamin Bortfeldt <bbortfeldt@gmail.com>
2013-01-25 14:03:48 +0100

    implemented statements for ItemsChangedEvent and WorkEvent - added missing trait

---
+++ src/main/scala/de/hsaugsburg/cep/agent/OntologyService.scala
@@ -0,0 +1,13 @@
+package de.hsaugsburg.cep.agent
+
+import de.hsaugsburg.cep.model.ChangeType
+import de.hsaugsburg.cep.model.Work
+
+trait OntologyService {
+	def isNeighbour(sensorId: String, otherSensorId: String): Boolean
+	def isMachineSensor(sensorId: String): Boolean
+	def getMachineId(sensorId: String): String
+	def getMachineWorkType(sensorId: String): Work.Work
+	def isChangeSensor(sensorId: String): Boolean
+	def getChangeSensorType(sensorId: String): ChangeType.ChangeType
+}
